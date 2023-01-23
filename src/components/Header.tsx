72e53654a53afd7d3eb66cb1914668e6eca0eee3
DanielFrancajp <danieldenotas@gmail.com>
2023-01-23 18:16:20 -0300

    atualização do modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,34 @@
 import { Plus } from "phosphor-react"
+import { useState } from "react";
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImage from '../assets/logo.svg'
 
 export function Header() {
+
+
+
     return (
         <div className='w-full max-w-3x1 mx-auto flex items-center justify-between'>
             <img src={logoImage} alt="Habits" />
 
-            <button type='button'
-                className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+            <Dialog.Root>
+                <Dialog.Trigger
+                    type='button'
+                    className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+                >
+                    <Plus size={20} className="text-violet-500" />
+                    novo hábito
+                </Dialog.Trigger>
+                
+                <Dialog.Portal>
+                    <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0"/>
 
-            >
-                <Plus size={20} className="text-violet-500" />
-                novo hábito
-            </button>
+                    <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                        Conteúdo do Modal
+                    </Dialog.Content>
+
+                </Dialog.Portal>
+            </Dialog.Root>
         </div>
 
     )
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -41,13 +41,11 @@ export function SummaryTable() {
                     return <HabitDay key={date.toString()} />
                 })}
 
-                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_,i) => {
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                     return (
 
-                        <div key ={i}className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                        <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
                         />
-
-                        
 
                     )
                 })}
